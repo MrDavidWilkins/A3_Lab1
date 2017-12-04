c27dea8f30768dbc6a9625cb3baa3665d44dd023
Wilkins <d_wilkins@tss-hr420-dm34.local>
2017-12-04 17:36:34 -0500

    working

---
+++ includes/functions.php
@@ -0,0 +1,41 @@
+<?php
+  $user = "root";
+  $pass = "root";
+  $host = "localhost";
+  $db = "db_cooperinfo";
+
+  $conn = mysqli_connect($host, $user, $pass, $db);
+  mysqli_set_charset($conn, 'utf8');
+
+  if (!$conn) {
+    echo "it's fucked, mate";
+    exit;
+  }
+
+  // echo "it's working";
+
+  $myQuery = "SELECT * FROM mainmodel";
+  $result = mysqli_query($conn, $myQuery);
+
+  $rows = array();
+
+  while($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+
+  // var_dump($rows);
+  // echo json_encode($rows);
+  //
+
+  if (isset($_GET['carModel'])) {
+    $car = $_GET['carModel'];
+
+    $myQuery = "SELECT * FROM mainmodel WHERE model = '$car'";
+    $result = mysqli_query($conn, $myQuery);
+
+    $row = mysqli_fetch_assoc($result);
+
+    echo json_encode($row);
+  }
+
+?>
